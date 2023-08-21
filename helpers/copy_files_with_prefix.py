b3e4a6daf713c069fe3da5b7ba5a12ef0bc1070f
Muhammad Shahzaib Minhas <mshahzaib1629@gmail.com>
2023-08-21 10:35:07 +0500

    labels.txt added on copying files

--- helpers/copy_files_with_prefix.py
+++ helpers/copy_files_with_prefix.py
@@ -1,23 +1,42 @@
 import os
 import shutil
+from collections import defaultdict
 
-# This function is supposed to copy images of given labels from one directory to another directory
 def copy_files_with_prefix(src_folder, dest_folder, prefixes):
-    # Get the list of files in the source folder
     file_list = os.listdir(src_folder)
+    copied_labels = defaultdict(int)  # Dictionary to store label counts
+    total_images_copied = 0
 
-    # Iterate over the files and copy those that start with any of the specified prefixes
     for file_name in file_list:
         for prefix in prefixes:
             if file_name.startswith(prefix):
                 src_path = os.path.join(src_folder, file_name)
                 dest_path = os.path.join(dest_folder, file_name)
                 shutil.copy(src_path, dest_path)
-                print(f"copied {file_name} from {src_folder} to {dest_folder}")
+                print(f"Copied {file_name} from {src_folder} to {dest_folder}")
+
+                label = file_name[:4]
+                copied_labels[label] += 1
+                total_images_copied += 1
+
+    labels_path = os.path.join(dest_folder, "labels.txt")
+    with open(labels_path, "w") as labels_file:
+        labels_file.write('\n\n=== Labels in this chunk =====\n\n')
+        for label, count in copied_labels.items():
+            labels_file.write(f'"{label}", ')  # Include label count
+
+        # adding summary
+        labels_file.write('\n\n=== Summary ========\n\n')
+        labels_file.write(f'Total Identities: {len(copied_labels.keys())}\n')  # Include label count 
+        labels_file.write(f'Total Images: {total_images_copied}\n\n')    
+        labels_file.write(f'Images per label -----\n\n')   
+        for label, count in copied_labels.items():
+            labels_file.write(f'"{label}": {count}\n')  # Include label count
 
 # Example usage:
-source_folder = "./reid-data/market1501-test/Market-1501-v15.09.15/bounding_box_test" 
-destination_folder = "./reid-data/market1501-test/Market-1501-v15.09.15/test_sets/s1"
-prefixes_to_copy = ["0001", "0004", "0000"]
+source_folder = "./reid-data/market1501-test/Market-1501-v15.09.15/bounding_box_train" 
+destination_folder = "./reid-data/market1501-test/Market-1501-v15.09.15/train_chunks/c2"
+prefixes_to_copy = ["0023", "0037", "0032", "0027", "0028", "0030", "0035", ]
 
 copy_files_with_prefix(source_folder, destination_folder, prefixes_to_copy)
+
