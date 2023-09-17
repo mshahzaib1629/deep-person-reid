d39ce8275520fb06fabb76cd63ab9da6bf139ee1
Muhammad Shahzaib Minhas <mshahzaib1629@gmail.com>
2023-09-17 19:35:46 +0500

    added training conditions in test_finetune

--- representative_memory/memory_loader.py
+++ representative_memory/memory_loader.py
@@ -3,8 +3,20 @@ import re
 import glob
 import os.path as osp
 import warnings
-
+import json, os
 from torchreid.data import ImageDataset
+
+
+def get_image_label_dict(representative_memory_directory):
+    labels_file_path = os.path.join(representative_memory_directory, "labels.json")
+
+    label_json_data = {}
+    if os.path.exists(labels_file_path):
+        with open(labels_file_path, "r") as json_file:
+            label_json_data = json.load(json_file)
+            json_file.close()
+
+    return label_json_data
 
 
 class RepresentativeMemory(ImageDataset):
@@ -12,14 +24,16 @@ class RepresentativeMemory(ImageDataset):
 
     _junk_pids = [0, -1]
     dataset_dir = "representative-memory"
-    
+
     def __init__(self, root="", **kwargs):
         self.root = osp.abspath(osp.expanduser(root))
         self.dataset_dir = osp.join(self.root, self.dataset_dir)
-        
+
         if osp.isdir(self.dataset_dir):
             self.data_dir = self.dataset_dir
         else:
+            os.makedirs(self.dataset_dir)
+            self.data_dir = self.dataset_dir
             warnings.warn("Representative Memory not found.")
 
         self.train_dir = self.data_dir
@@ -31,12 +45,11 @@ class RepresentativeMemory(ImageDataset):
         self.check_before_run(required_files)
 
         train = self.process_dir(self.train_dir, relabel=True)
-        query =[]
+        query = []
         gallery = []
 
-        print('train len: ', len(train))
-        # query.append(train[0])
-        # gallery.append(train[0])
+        # print('train [0]: ', train[0])
+        # print('train [1]: ', train[1])
 
         super(RepresentativeMemory, self).__init__(train, query, gallery, **kwargs)
 
@@ -44,24 +57,34 @@ class RepresentativeMemory(ImageDataset):
         img_paths = glob.glob(osp.join(dir_path, "*.jpg"))
         pattern = re.compile(r"([-\d]+)_c(\d)")
 
+        labels_dict = get_image_label_dict(dir_path)
+
         pid_container = set()
         for img_path in img_paths:
-            pid, _ = map(int, pattern.search(img_path).groups())
+            # pid, _ = map(int, pattern.search(img_path).groups())
+            file_name = img_path.split("/")[-1]
+            pid = int(labels_dict[file_name])
+
             if pid == -1:
                 continue  # junk images are just ignored
             pid_container.add(pid)
         pid2label = {pid: label for label, pid in enumerate(pid_container)}
 
+        # print("pid2label: ", pid2label)
         data = []
         for img_path in img_paths:
-            pid, camid = map(int, pattern.search(img_path).groups())
+            # pid, camid = map(int, pattern.search(img_path).groups())
+            file_name = img_path.split("/")[-1]
+            pid = int(labels_dict[file_name])
             if pid == -1:
                 continue  # junk images are just ignored
-            assert 0 <= pid <= 1501  # pid == 0 means background
-            assert 1 <= camid <= 6
-            camid -= 1  # index starts from 0
+            # @TODO: Need to discuss if we need camera info
+            # assert 0 <= pid <= 1501  # pid == 0 means background
+            # assert 1 <= camid <= 6
+            # camid -= 1  # index starts from 0
             if relabel:
                 pid = pid2label[pid]
-            data.append((img_path, pid, camid))
+            print(file_name, " => ", pid)
+            data.append((img_path, pid, 0))
 
         return data
--- test_finetune.py
+++ test_finetune.py
@@ -1,18 +1,24 @@
 import torchreid
-
+import os
 from torchsummary import summary
 from representative_memory import (
     apply_herding_selection,
     get_representative_images,
     RepresentativeMemory,
 )
+
 torchreid.data.register_image_dataset("representative_memory", RepresentativeMemory)
 
 
 def run_reid():
+    weight_directory = "log/resnet50/model/model.pth.tar-1"
+    representative_memory_directory = "reid-data/representative-memory"
+    should_update: bool = os.path.exists(weight_directory) and os.path.exists(
+        representative_memory_directory
+    )
     datamanager = torchreid.data.ImageDataManager(
         root="reid-data",
-        sources=["representative_memory"],
+        sources=["market1501Test", "representative_memory"],
         targets="market1501Test",
         height=256,
         width=128,
@@ -30,15 +36,6 @@ def run_reid():
 
     optimizer = torchreid.optim.build_optimizer(model, optim="adam", lr=0.0003)
 
-    print("model summary before pretained_weights: ")
-    print(summary(model, (3, 128, 256), 32))
-
-    torchreid.utils.load_pretrained_weights(model, "log/resnet50/model/model.pth.tar-1")
-
-    print("model summary after pretained_weights: ")
-    print(summary(model, (3, 128, 256), 32))
-    # return
-
     scheduler = torchreid.optim.build_lr_scheduler(
         optimizer, lr_scheduler="single_step", stepsize=20
     )
@@ -47,14 +44,35 @@ def run_reid():
         datamanager, model, optimizer=optimizer, scheduler=scheduler, label_smooth=True
     )
 
-    engine.run(
-        save_dir="log/resnet50",
-        max_epoch=1,
-        eval_freq=10,
-        print_freq=2,
-        test_only=False,
-        fixbase_epoch=5,
-        open_layers="classifier",
+    if should_update:
+        torchreid.utils.load_pretrained_weights(model, weight_directory)
+
+        engine.run(
+            save_dir="log/resnet50",
+            max_epoch=1,
+            eval_freq=10,
+            print_freq=2,
+            test_only=False,
+            fixbase_epoch=5,
+            open_layers="classifier",
+        )
+    else:
+        engine.run(
+            save_dir="log/resnet50",
+            max_epoch=1,
+            eval_freq=10,
+            print_freq=2,
+            test_only=False,
+        )
+
+    # @TODO: Fix Needed: this function also take rp memory as input from train loader
+    apply_herding_selection(
+        train_loader=datamanager.train_loader,
+        representative_memory_directory=representative_memory_directory,
+        label_start_index=0,
+        label_end_index=3,
+        selection_percent=0.5,
+        retain_percent=0.5,
     )
 
 
