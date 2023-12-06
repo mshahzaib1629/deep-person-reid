8b311297315b1377c610fa70faa24f7ac6da82e5
Muhammad Shahzaib Minhas <mshahzaib1629@gmail.com>
2023-12-06 16:39:57 +0500

    saving model_eval graph

--- graphs/model_eval.py
+++ graphs/model_eval.py
@@ -1,4 +1,4 @@
-import sys
+import sys, re
 sys.path.append("/home/code/Shahzaib/MS/Thesis/Implementation/deep-person-reid/")
 import pandas as pd
 import plotly.graph_objects as go
@@ -10,14 +10,14 @@ from helpers import Matric, SelectedDatasets
 data = {
     "models_trained": ["r12", "r24", "r25"],
     "market1501": {
-        "mAP": [39.48, 44.6, 38.9],
-        "Rank-1": [66.1, 68.5, 63.4],
-        "Rank-5": [83.9, 84.6, 81.7]
+        "mAP": ["39.48%", "44.6%", "38.9%"],
+        "Rank-1": ["66.1%", "68.5%", "63.4%"],
+        "Rank-5": ["83.9%", "84.6%", "81.7%"]
     },
     "dukemtmcreid": {
-        "mAP": [10.9, 24.1, 23.3],
-        "Rank-1": [22.2, 43.9, 45.01],
-        "Rank-5": [36.7, 61.1, 63.8]
+        "mAP": ["10.9%", "24.1%", "23.3%"],
+        "Rank-1": ["22.2%", "43.9%", "45.01%"],
+        "Rank-5": ["36.7%", "61.1%", "63.8%"]
     }
 }
 
@@ -68,11 +68,32 @@ def _get_data(rows, datasets, matrices):
                     add_data(dataset, Matric.rank5, row, 'K') if Matric.rank5 in matrices else None
                     add_data(dataset, Matric.rank10, row, 'L') if Matric.rank10 in matrices else None
                     add_data(dataset, Matric.rank20, row, 'M') if Matric.rank20 in matrices else None
-    print(data)
+    # print(data)
     return data
 
-
+def _save_graph(data, fig): 
     
+    def make_file_name():
+        models_sorted = sorted(data['models_trained'])
+        datasets_sorted_list = sorted(DATASETS)
+        matrices_sorted_list = sorted(MATRICES)
+
+        file_name = "-".join([f"{model}" for model in models_sorted]) + "_" + "-".join([f"{dataset}" for dataset in datasets_sorted_list]) + "_" + "-".join(f"{matric}" for matric in matrices_sorted_list)
+        return file_name
+    
+    # worksheet name without Analysis keyword
+    main_worksheet_name = re.sub(r'\[.*?\]', '', WORKSHEET_NAME).strip()
+    directory =  os.path.join(".", "training_results", main_worksheet_name)
+    
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    analysis_path = os.path.join(directory, 'analysis')
+    if not os.path.exists(analysis_path):
+        os.makedirs(analysis_path)
+    
+    file_path = os.path.join(analysis_path, f'{make_file_name()}.html')
+    fig.write_html(file_path, auto_open=True)
 
 def plot_graph(data, rows, metrics, datasets):
     
@@ -97,20 +118,46 @@ def plot_graph(data, rows, metrics, datasets):
                 name=f'{dataset} {metric}'
             ))
 
+    def make_title(WORKSHEET_NAME, rows):
+        models_sorted = sorted(data['models_trained'])
+        datasets_sorted_list = sorted(DATASETS)
+        matrices_sorted_list = sorted(MATRICES)
+        # Sort the list
+        sorted_list = sorted(rows)
+
+        # Create the text
+        title = f"{WORKSHEET_NAME} - {', '.join(map(str, models_sorted))} - {', '.join(map(str, datasets_sorted_list))} - {', '.join(map(str, matrices_sorted_list))} "
+        return title
+    
     # Update the layout of the figure
     fig.update_layout(
-        title='Evaluation Metrics Line Graph',
+        title='Trained Models Perfornmace on Datasets',
         xaxis_title='Models Trained',
         yaxis_title='Metric Values',
-        legend_title='Metrics'
+        legend_title='Metrics',
+        annotations=[
+        dict(
+            text=make_title(WORKSHEET_NAME, TARGET_ROWS),
+            showarrow=False,
+            xref="paper",
+            yref="paper",
+            x=0.006,  
+            y=1.05,  
+            xanchor="left",
+            yanchor="top",  
+            font=dict(
+                size=14,
+                color="black"  
+            )
+        )]
     )
 
-    fig.show()
+    _save_graph(data, fig)
  
 
 WORKSHEET_NAME = "Test [Analysis] Finetune with RP - ResNet18"
 TARGET_ROWS = [ 3, 4,]
-MATRICES = [ Matric.rank1]
+MATRICES = [ Matric.rank5, Matric.rank1, Matric.map]
 DATASETS = [SelectedDatasets.Market1501, SelectedDatasets.DukeMTMC]
 
 # data = _get_data(TARGET_ROWS, DATASETS, MATRICES)
